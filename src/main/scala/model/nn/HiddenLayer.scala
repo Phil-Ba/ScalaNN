246da972600fb3362eae9b91d5825a5c29fb8d6d
philba <p.bayer@freemail.hu>
2018-01-29 21:53:43 +0100

    extending proper base class now

--- src/main/scala/model/nn/HiddenLayer.scala
+++ src/main/scala/model/nn/HiddenLayer.scala
@@ -2,7 +2,7 @@ package model.nn
 
 import breeze.linalg.DenseMatrix
 
-class HiddenLayer(private val thetas: DenseMatrix[Double]) extends Layer {
+class HiddenLayer(override val thetas: DenseMatrix[Double]) extends ConnectableLayer {
 
   val units = thetas.rows
   val inputs = thetas.cols - 1
