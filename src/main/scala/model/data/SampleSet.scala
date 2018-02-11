f6c66dd36d2a0cec58947ac3faa334d3aee3266a
philba <p.bayer@freemail.hu>
2018-02-11 18:24:06 +0100

    helper to sample datasets

---
+++ src/main/scala/model/data/SampleSet.scala
@@ -0,0 +1,12 @@
+package model.data
+
+import org.nd4j.linalg.api.ndarray.INDArray
+
+/**
+  *
+  */
+case class SampleSet(trainingSet: INDArray, trainingResultSet: INDArray,
+                     cvSet: INDArray, cvResultSet: INDArray,
+                     testSet: INDArray, testResultSet: INDArray) {
+
+}
