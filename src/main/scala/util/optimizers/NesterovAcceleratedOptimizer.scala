f375d40e0c6696fb3e66d3662dc8a9e592ae4462
philba <p.bayer@freemail.hu>
2018-02-26 22:19:04 +0100

    reimplemented according to lecture found online

--- src/main/scala/util/optimizers/NesterovAcceleratedOptimizer.scala
+++ src/main/scala/util/optimizers/NesterovAcceleratedOptimizer.scala
@@ -4,37 +4,43 @@ import com.typesafe.scalalogging.StrictLogging
 import model.nn.InputLayer
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
+import org.nd4s.Implicits.RichINDArray
 import util.{CostFunction, NNRunner}
 
 /**
-  *
+  * Implementation according to http://cs231n.github.io/neural-networks-3/#sgd
   */
 object NesterovAcceleratedOptimizer extends StrictLogging {
 
-  def minimize(x: INDArray, y: INDArray, inputLayer: InputLayer, iterations: Int = 100, lambda: Double = 0, learnRate: Double = 1D) = {
+  def minimize(x: INDArray, y: INDArray, inputLayer: InputLayer, iterations: Int = 100
+               , lambda: Double = 0, learnRate: Double = 1D, momentum: Double = 0.9) = {
 
-    val gamma = 0.9
-    var v = inputLayer.getNNThetas
+    var vtMinusOne = inputLayer.getNNThetas
       .map(Nd4j.zerosLike(_))
+    var vt = inputLayer.getNNThetas
+      .map(Nd4j.zerosLike(_))
+
     val costs = (1 to iterations).map { i =>
+      val update = vtMinusOne.zip(vt)
+        .map { case (vtMinusOneLayer, vtLayer) =>
+          vtMinusOneLayer.muli(momentum).subi(vtLayer * (1 + momentum))
+        }
+      inputLayer.updateWithGradients(update)
+
       val (yCalc, gradients) = NNRunner.runWithData(x, y, inputLayer, lambda)
       val cost = CostFunction.cost(yCalc, y, inputLayer.getNNThetas, lambda)
       logger.info("Iteration[{}] cost:[{}]", i, cost)
-      //      if (learnRate != 1) {
-      //        gradients.foreach(_ *= learnRate)
-      //      }
 
-      val vtNew = v.zip(gradients).map { (vtGrad) =>
-        val (vt, gradients) = vtGrad
-        vt.mul(gamma).subi(gradients.mul(learnRate))
+      vtMinusOne = vt
+
+      if (learnRate != 1) {
+        gradients.foreach(_ *= learnRate)
       }
-      val update = v.zip(vtNew)
-        .map { vVnew =>
-          val (v, vtNew) = vVnew
-          v.mul(gamma).addi(vtNew.mul(-gamma - 1))
-        }
-      inputLayer.updateWithGradients(update)
-      v = vtNew
+
+      vt = vt.zip(gradients).map { case (vtLayer, grads) =>
+        (vtLayer * momentum) -= grads
+      }
+
       cost
     }
     costs
