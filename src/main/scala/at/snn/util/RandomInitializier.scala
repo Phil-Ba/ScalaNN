e5a7d2a3dc628911ec705e0238ef928f786f1656
philba <p.bayer@freemail.hu>
2018-03-15 09:10:57 +0100

    more sensible default

--- src/main/scala/at/snn/util/RandomInitializier.scala
+++ src/main/scala/at/snn/util/RandomInitializier.scala
@@ -9,10 +9,8 @@ import org.nd4s.Implicits._
   */
 object RandomInitializier {
 
-  def initialize(layerSize: Int, intputs: Int, epsilon: Double = 5): INDArray = {
-
-    Nd4j.rand(layerSize, intputs + 1) * (2 * epsilon) - epsilon
-
+  def initialize(layerSize: Int, inputs: Int, epsilon: Double = 1): INDArray = {
+    Nd4j.rand(layerSize, inputs + 1) * (2 * epsilon) - epsilon
   }
 
 }
