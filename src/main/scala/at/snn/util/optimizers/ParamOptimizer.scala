1373363f4f00ae5de63f0799dc9b2e90ca68b9f6
philba <p.bayer@freemail.hu>
2018-03-15 23:57:47 +0100

    changed api to seq of values and added method for 2 params

--- src/main/scala/at/snn/util/optimizers/ParamOptimizer.scala
+++ src/main/scala/at/snn/util/optimizers/ParamOptimizer.scala
@@ -10,8 +10,8 @@ import at.snn.util.{CostFunction, NNRunner}
 object ParamOptimizer {
 
   def optimizeByPredictions(dataSet: SampleSet, network: InputLayer, function: (InputLayer, Double) => Any,
-                            from: Double = 0.5, to: Double = 1.0, stepSize: Double = 0.3): (Double, Double) = {
-    (from to to by stepSize).map { p =>
+                            paramValues: Seq[Double]): (Double, Double) = {
+    paramValues.map { p =>
       val nn = network.copyNetwork
       function(nn, p)
       val correctPercent = NNRunner.runPredictions(nn, dataSet.cvSet, dataSet.cvResultSet).correctPercent
@@ -19,9 +19,23 @@ object ParamOptimizer {
     }.maxBy(_._2)
   }
 
+  def optimizeByPredictions(dataSet: SampleSet, network: InputLayer, function: (InputLayer, Double, Double) => Any,
+                            param1Values: Seq[Double], param2Values: Seq[Double]): (Double, Double, Double) = {
+    val results = for {
+      p1 <- param1Values
+      p2 <- param2Values
+    } yield {
+      val nn = network.copyNetwork
+      function(nn, p1, p2)
+      val correctPercent = NNRunner.runPredictions(nn, dataSet.cvSet, dataSet.cvResultSet).correctPercent
+      (p1, p2, correctPercent)
+    }
+    results.maxBy(_._3)
+  }
+
   def optimizeByCost(dataSet: SampleSet, network: InputLayer, function: (InputLayer, Double) => Any,
-                     from: Double = 0.5, to: Double = 1.0, stepSize: Double = 0.3): (Double, Double) = {
-    (from to to by stepSize).map { p =>
+                     paramValues: Seq[Double]): (Double, Double) = {
+    paramValues.map { p =>
       val nn = network.copyNetwork
       function(nn, p)
       val calcY = NNRunner.runWithData(dataSet.cvSet, nn)
@@ -30,4 +44,18 @@ object ParamOptimizer {
     }.minBy(_._2)
   }
 
+  def optimizeByCost(dataSet: SampleSet, network: InputLayer, function: (InputLayer, Double, Double) => Any,
+                     param1Values: Seq[Double], param2Values: Seq[Double]): (Double, Double, Double) = {
+    val results = for {
+      p1 <- param1Values
+      p2 <- param2Values
+    } yield {
+      val nn = network.copyNetwork
+      function(nn, p1, p2)
+      val calcY = NNRunner.runWithData(dataSet.cvSet, nn)
+      val cost = CostFunction.cost(calcY, dataSet.cvResultSet)
+      (p1, p2, cost)
+    }
+    results.minBy(_._3)
+  }
 }
