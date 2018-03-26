92066a914749d22c0bb8c7fec252df1ab4db3cd9
philba <p.bayer@freemail.hu>
2018-03-26 18:39:14 +0900

    Helper class to convert strings to labels

---
+++ src/main/scala/at/snn/util/data/TextToDoubleConverter.scala
@@ -0,0 +1,18 @@
+package at.snn.util.data
+
+import scala.collection.mutable
+
+/**
+  * Converts textual values into double values, by assigning the first occurrence of a string a number.
+  * All subsequent occurrences will receive the same number value. This will only be useful if your values are
+  * some enumerated strings.
+  */
+class TextToDoubleConverter {
+
+  private val values: mutable.Map[String, Double] = new mutable.HashMap[String, Double]
+
+  def convertToDouble(inp: String) = {
+    values.getOrElseUpdate(inp, values.maxBy(_._2)._2 + 1)
+  }
+
+}
